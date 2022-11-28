c8f111c35a5cf6920577f475d57ad057a304abdb
Nerms <Nerms@Nermins-MacBook-Pro.local>
2022-11-27 23:52:42 -0600

    added function to save and a funciton that calls for searched cities

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-let key = '640a83e1973db525d8014b10ebf30875'
+var key = '640a83e1973db525d8014b10ebf30875'
 
-let userCity = "";
+var userCity = "";
 
 var currentWeather = function (userCity) {
 
-  let apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userCity}&appid=`+ key;
+  var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userCity}&appid=`+ key;
   
   fetch(apiURL)
     .then(function (response) {
@@ -19,3 +19,25 @@ var currentWeather = function (userCity) {
     })
 }
 
+var searchedCity = JSON.parse(localStorage.getItem('City')) || [];
+
+    for (var i = 0; i < searchedCity.length; i++) {
+        var historyButton = document.createElement('button');
+        historyButton.setAttribute('class', 'userCities');
+        historyButton.textContent = searchedCity[i];
+        $('#searchedCity').append(historyButton); 
+        searchedCitiesButton();
+}
+
+function searchedCitiesButton() {
+
+    var searchedCityButton = document.querySelectorAll('.userCity');
+    searchedCityButton.forEach(function (buttons) {
+      buttons.addEventListener('click', function (examp) {
+        userCity = examp.target.innertext;
+  
+        currentWeather(userCity);
+      });
+    });
+  }
+  
